d1f736c4fde39ac61a39e93be98887c45d065396
rsm8747 <moray8747@gmail.com>
2024-02-13 14:19:45 +0530

    change

--- delete.php
+++ delete.php
@@ -17,7 +17,7 @@
     <h2>Delete Record</h2>
     <form class="post-form" action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
         <div class="form-group">
-            <label>Id</label>
+            <label>ID</label>
             <input type="text" name="sid" />
         </div>
         <input class="submit" type="submit" name="deletebtn" value="Delete" />
